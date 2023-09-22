70e3e3e6ab29dd3f29b111a364a000415e0b98af
Sajid_Alee <134279095+SajidAleeDev@users.noreply.github.com>
2023-09-22 20:27:08 +0500

    Update Request.jsx

--- src/Request.jsx
+++ src/Request.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_KEY = "49249c67676503688e912586c3520caf";
+const API_KEY = "<Your api Key from TMbd>";
 
 const Request = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
